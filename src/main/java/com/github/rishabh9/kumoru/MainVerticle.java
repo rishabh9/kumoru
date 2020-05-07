4047e33e67df4cccaecdf92c1c274bc54d1a4f54
RISHABH JOSHI <rishabh9@gmail.com>
2020-05-07 19:24:04 +0530

    Update build configuration
    1. Updated build configuration
    2. Added logging configuration
    3. Formatted content

--- src/main/java/com/github/rishabh9/kumoru/MainVerticle.java
+++ src/main/java/com/github/rishabh9/kumoru/MainVerticle.java
@@ -1,26 +1,28 @@
 package com.github.rishabh9.kumoru;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class MainVerticle extends AbstractVerticle {
 
   private static final int PORT = 8888;
 
   @Override
-  public void start(final Future<Void> startFuture) throws Exception {
+  public void start(final Promise<Void> startFuture) throws Exception {
     vertx
         .createHttpServer()
         .requestHandler(
             req -> {
-              req.response().putHeader("content-type", "text/plain").end("Hello from Vert.x!");
+              req.response().putHeader("content-type", "text/plain").end(req.path());
             })
         .listen(
             PORT,
             http -> {
               if (http.succeeded()) {
                 startFuture.complete();
-                System.out.println("HTTP server started on port 8888");
+                log.info("HTTP server started on port 8888");
               } else {
                 startFuture.fail(http.cause());
               }
